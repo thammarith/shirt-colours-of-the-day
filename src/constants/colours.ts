b70ea22f68bb7ed926180a8e8d941f4f6ae78487
Thammarith <8011543+thammarith@users.noreply.github.com>
2022-04-06 17:34:47 +0700

    add repo

---
+++ src/constants/colours.ts
@@ -0,0 +1,15 @@
+export type Colour = { en: string; th: string };
+
+export const blue = { en: 'blue', th: 'น้ำเงิน' };
+export const black = { en: 'black', th: 'ดำ' };
+export const brown = { en: 'brown', th: 'น้ำตาล' };
+export const cream = { en: 'cream', th: 'ครีม' };
+export const green = { en: 'green', th: 'เขียว' };
+export const grey = { en: 'grey', th: 'เทา' };
+export const lightBlue = { en: 'light-blue', th: 'ฟ้า' };
+export const orange = { en: 'orange', th: 'ส้ม' };
+export const pink = { en: 'pink', th: 'ชมพู' };
+export const purple = { en: 'purple', th: 'ม่วง' };
+export const red = { en: 'red', th: 'แดง' };
+export const white = { en: 'white', th: 'ขาว' };
+export const yellow = { en: 'yellow', th: 'เหลือง' };
---
+++ src/constants/days.ts
@@ -0,0 +1,9 @@
+export type Day = { id: number; en: string; th: string };
+
+export const sunday = { id: 0, en: 'sunday', th: 'วันอาทิตย์' };
+export const monday = { id: 1, en: 'monday', th: 'วันจันทร์' };
+export const tuesday = { id: 2, en: 'tuesday', th: 'วันอังคาร' };
+export const wednesday = { id: 3, en: 'wednesday', th: 'วันพุธ' };
+export const thursday = { id: 4, en: 'thursday', th: 'วันพฤหัสบดี' };
+export const friday = { id: 5, en: 'friday', th: 'วันศุกร์' };
+export const saturday = { id: 6, en: 'saturday', th: 'วันเสาร์' };
